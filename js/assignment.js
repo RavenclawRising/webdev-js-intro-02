1cf5f51472e4aec687bc6bc20e178d72ade52892
unknown <Lauradu@me.com>
2024-12-17 17:26:20 -0500

    Updated three files. Practiced with mutable and immutable variables in index.js. Added resolution, year, and boolean variable. Updated README.

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,35 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = "Get three coding language certifications.";
+const currentYear = 2024;
+let willMeetResolution = true;
 
 function updateYear() {
     // Update this function
+    yearElement.innerText = 2024;
 
 }
 
 function updateResolution() {
     // Update this function 
-
+    resolutionElement.innerText = "Get three coding language certifications.";
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+    willMeetResolutionElement.innerText = true;
 }
 
 function render() {
     // Update this function
-
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
+    willMeet
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+    render ();
 })
--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@
 // that are not declared
 "use strict"
 
+/*
+
+const myDOB = 0129;
+console.log(myDOB);                    //  Output: 129
+console.log(typeof myDOB);             //  Output: "number"
+
+const job = "teacher";             
+console.log(job);                      //  Output: "teacher"
+console.log(typeof job);               //  Output: "string"
+
+let siblings = 3;
+console.log(siblings);                 //  Output: 3
+console.log(typeof siblings);          //  Output: "number"
+
+let jobSatisfaction = false;
+console.log(jobSatisfaction);          //  Output: false
+console.log(typeof jobSatisfaction);   //  Output: "boolean"
+
+*/
